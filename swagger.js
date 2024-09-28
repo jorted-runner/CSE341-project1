bc59de312f4fce27aa2718be832b386e386b06d0
jorted-runner <jorted.runner1996@gmail.com>
2024-09-28 16:31:27 -0500

    update docs

--- swagger.js
+++ swagger.js
@@ -1,13 +1,13 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-    info: {
-        title: 'Contacts API',
-        description: "Contact Management API"
-    },
-    host: 'localhost:8080',
-    schemas: ['https', 'http']
-}
+	info: {
+		title: 'Contacts API',
+		description: 'Contact Management API',
+	},
+	host: 'cse341-project1-ascp.onrender.com',
+	schemas: ['https', 'http'],
+};
 
 const outputFile = './swagger.json'
 const endpointFiles = ['./routes/contacts.js', './routes/index.js']
